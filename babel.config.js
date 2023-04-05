dffa1b04da50a853f64dff0d5f1803fba07279d2
Daniil Borysov <danofu13@gmail.com>
2023-04-05 16:22:44 +0200

    Environment setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        { root: ['src', 'assets'], extensions: ['.js', '.android.js', '.ios.js', '.jsx', '.android.jsx', '.ios.jsx'] },
+      ],
+    ],
   };
 };
